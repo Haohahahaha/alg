19e8e0241ae05797c6abf7893ccf0af6ea25ce18
Haohahahaha_ <1259203802@qq.com>
2022-08-19 18:16:42 +0800

    2022-08-19_18:16:42 update

---
+++ docs/CodeThinker/5/code/503/TwoSum.h
@@ -0,0 +1,15 @@
+#ifndef _TWOSUM_H_
+#define _TWOSUM_H_
+
+#include <iostream>
+#include <vector>
+
+class TwoSum {
+public:
+    vector<int> twoSum(vector<int>& nums, int target);
+}
+
+
+
+
+#endif
